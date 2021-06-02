8849cfedc32b658fe807522b95ee83987cf73031
Finnerspx <finleypurnell98@gmail.com>
2021-06-02 09:22:25 +0100

    Tring new prog style

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainDashboard from './MainDashboard';
+
+
+function App() {
+  return (
+    <div>
+      <MainDashboard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navigation/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div className="w-full h-20 bg-black">
+            <div className="pt-5 pl-10 ">
+                <h1 className="text-2xl text-blue-200">G5 - <a className="text-blue-500">Application</a></h1>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
